6b3118fba64d95c213dd8739caac93f140b2ae03
anika-gupta-8 <anigupta@uw.edu>
2024-03-05 12:15:43 -0800

    Python file for stimulus package

---
+++ stimulus_package.py
@@ -0,0 +1,12 @@
+##Jacob Cavon, Anika Gupta, Ariell, 2024##
+##GO term analsyis for enriched BFF genes.##
+
+# Import packages
+
+import random
+from psychopy import core, sound, visual
+import time
+from gtts import gTTS
+import os
+from psychopy.sound import Sound
+import psychtoolbox as ptb
